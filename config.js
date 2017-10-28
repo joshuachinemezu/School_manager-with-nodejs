74792a4ccd97213e9d4b405b0062d2cb1cc5904f
DevJossy <Joshuachinemezu@gmail.com>
2017-10-28 14:18:22 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
     database: {
-        host: 'us-cdbr-iron-east-05', 	// database host
+        host: 'us-cdbr-iron-east-05.cleardb.net', 	// database host
         user: 'b737688e0241c0', 		// your database username
         password: '5b7b158d', 		// database password
         db: 'heroku_32d4a49c0714ef0' 		// your database name
