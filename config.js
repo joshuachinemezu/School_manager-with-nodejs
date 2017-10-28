98b04d68b3746022371a7277467949d055e7470f
DevJossy <Joshuachinemezu@gmail.com>
2017-10-28 14:14:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var config = {
     database: {
         host: 'us-cdbr-iron-east-05', 	// database host
-        user: '	b737688e0241c0', 		// your database username
-        password: '	5b7b158d', 		// database password
-        db: ' heroku_32d4a49c0714ef0' 		// your database name
+        user: 'b737688e0241c0', 		// your database username
+        password: '5b7b158d', 		// database password
+        db: 'heroku_32d4a49c0714ef0' 		// your database name
     },
     server: {
         host: '127.0.0.1',
