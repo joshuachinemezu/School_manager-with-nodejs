78e38563444c4c899b7d877e940a28b2550d40da
DevJossy <Joshuachinemezu@gmail.com>
2017-10-28 13:20:49 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 var config = {
     database: {
-        host: '127.0.0.1', 	// database host
-        user: 'root', 		// your database username
-        password: '', 		// your database password
-        port: 3306, 		// default MySQL port
-        db: 'sch_mngr' 		// your database name
+        host: 'us-cdbr-iron-east-05', 	// database host
+        user: '	b737688e0241c0', 		// your database username
+        password: '	5b7b158d', 		// your database password
+        db: ' heroku_32d4a49c0714ef0' 		// your database name
     },
     server: {
         host: '127.0.0.1',
-        port: '8080'
+        port: '5000'
     }
 }
 
