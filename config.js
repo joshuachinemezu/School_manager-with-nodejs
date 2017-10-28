a5a9e15e0a38de1b89a120a0a74c1fa338192cb3
DevJossy <Joshuachinemezu@gmail.com>
2017-10-28 20:59:04 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
 var config = {
     database: {
-        host: 'us-cdbr-iron-east-05.cleardb.net', 	// database host
-        user: 'b737688e0241c0', 		// your database username
-        password: '5b7b158d', 		// database password
-        db: 'heroku_32d4a49c0714ef0' 		// your database name
+        host: '127.0.0.1', 	// database host
+        user: 'root', 		// your database username
+        password: '', 		// your database password
+        port: 3306, 		// default MySQL port
+        db: 'sch_mngr' 		// your database name
     },
     server: {
-        host: 'localhost',
-        port: '5000'
+        host: '127.0.0.1',
+        port: '8080'
     }
 }
 
